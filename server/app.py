8fb55ac0dabd804da2f53c0b5eceb1643d9e6cdc
annemusau <moigeanne646@gmail.com>
2024-10-09 01:53:45 +0300

    commiting chatteerbox lab

--- server/app.py
+++ server/app.py
@@ -22,5 +22,36 @@ def messages():
 def messages_by_id(id):
     return ''
 
+@app.route('/messages', methods=['GET'])
+def get_messages():
+    messages = Message.query.order_by(Message.created_at.asc()).all()
+    return jsonify([message.to_dict() for message in messages]), 200
+
+@app.route('/messages', methods=['POST'])
+def create_message():
+    data = request.get_json()
+    new_message = Message(
+        body=data.get('body'),
+        username=data.get('username')
+    )
+    db.session.add(new_message)
+    db.session.commit()
+    return jsonify(new_message.to_dict()), 201
+
+@app.route('/messages/<int:id>', methods=['PATCH'])
+def update_message(id):
+    message = Message.query.get_or_404(id)
+    data = request.get_json()
+    message.body = data.get('body', message.body)
+    db.session.commit()
+    return jsonify(message.to_dict()), 200
+
+@app.route('/messages/<int:id>', methods=['DELETE'])
+def delete_message(id):
+    message = Message.query.get_or_404(id)
+    db.session.delete(message)
+    db.session.commit()
+    return jsonify({"message": "Message deleted"}), 200
+
 if __name__ == '__main__':
     app.run(port=5555)
